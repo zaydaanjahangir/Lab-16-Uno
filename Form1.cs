15c15516c32b1a611ce757811f403030864409ff
zayj27 <91241397+zayj27@users.noreply.github.com>
2022-11-14 11:15:35 -0600

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComputerDeck3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComputerDeck1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComputerDeck2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComputerDeck4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComputerDeck5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComputerDeck6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComputerDeck7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YourDeck1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YourDeck2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YourDeck3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YourDeck4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YourDeck5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YourDeck6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YourDeck7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
